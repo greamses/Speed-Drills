a49767846b2273e592f778501c80260d6070bfdf
greamses <eemadanyel@gmail.com>
2025-10-23 06:33:16 +0100

    update main.js

--- Geometry/main.js
+++ Geometry/main.js
@@ -1,10 +1,10 @@
 import listConverter from '../list.js'
 
 const resources = [
-  { name: "Unskewed Flat Shapes", url: "./html/basic.html" },
-  { name: "Skewed Flat Shapes", url: "./html/skew.html" },
-  { name: "Skewed Flat Shapes 2", url: "./html/skew2.html" },
-  { name: "Circular Flat Shapes", url: "./html/circular.html" },
+  { name: "Unskewed Shapes", url: "./html/basic.html" },
+  { name: "Skewed Shapes", url: "./html/skew.html" },
+  { name: "Skewed Shapes 2", url: "./html/skew2.html" },
+  { name: "Circular Shapes", url: "./html/circular.html" },
 ];
 
 listConverter(resources)
