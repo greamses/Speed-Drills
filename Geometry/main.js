17491919ded493297c76640cd1b837175aa8c1a1
greamses <eemadanyel@gmail.com>
2025-11-06 05:44:00 +0100

    update

--- Geometry/main.js
+++ Geometry/main.js
@@ -3,6 +3,7 @@ import listConverter from '../js/list.js'
 const resources = [
   { name: "Unskewed Shapes", url: "./html/basic.html" },
   { name: "Skewed Shapes", url: "./html/skew.html" },
+  { name: "Skewed Shapes2", url: "./html/skew2.html" },
   { name: "Circular Shapes", url: "./html/circular.html" },
 ];
 
