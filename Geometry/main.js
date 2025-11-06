f409d121129ba7678b2e49bed019e82df53ac14c
greamses <eemadanyel@gmail.com>
2025-11-06 05:25:32 +0100

    update

--- Geometry/main.js
+++ Geometry/main.js
@@ -1,9 +1,8 @@
-import listConverter from '../list.js'
+import listConverter from '../js/list.js'
 
 const resources = [
   { name: "Unskewed Shapes", url: "./html/basic.html" },
   { name: "Skewed Shapes", url: "./html/skew.html" },
-  { name: "Skewed Shapes 2", url: "./html/skew2.html" },
   { name: "Circular Shapes", url: "./html/circular.html" },
 ];
 
---
+++ js/main.js
@@ -0,0 +1,10 @@
+import listConverter from './list.js'
+
+const resources = [
+  { name: "Arithmetics", url: "./Arithmetics/arithmetics.html" },
+  { name: "Algebra", url: "../Algebra/algebra.html" },
+  { name: "Geometry", url: "./Geometry/geometry.html" },
+  { name: "Tables", url: "./Tables/tables.html" }
+];
+
+listConverter(resources)
