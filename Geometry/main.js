74cb11d36d7e450726ace0e00d3343e6eaf18760
greamses <eemadanyel@gmail.com>
2025-11-19 16:29:52 +0100

    update main.js

--- Geometry/main.js
+++ Geometry/main.js
@@ -5,6 +5,7 @@ const resources = [
   { name: "Skewed Shapes", url: "./html/skew.html" },
   { name: "Skewed Shapes2", url: "./html/skew2.html" },
   { name: "Circular Shapes", url: "./html/circular.html" },
+  { name: "Triangle Interior", url: "./html/triangle-angles.html" },
 ];
 
 listConverter(resources)
